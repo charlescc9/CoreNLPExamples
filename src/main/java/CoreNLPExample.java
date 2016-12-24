74fbcc0f51b9c2e74301be7a85a8ee294eefd45f
Charles <charlescc9@yahoo.com>
2016-12-24 12:15:09 -0700

    Changed sample text

--- src/main/java/CoreNLPExample.java
+++ src/main/java/CoreNLPExample.java
@@ -1,7 +1,7 @@
 // Exploration of the Stanford CoreNLP API: stanfordnlp.github.io/CoreNLP/api
 
 import edu.stanford.nlp.dcoref.CorefCoreAnnotations;
-import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreAnnotations.*;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.naturalli.NaturalLogicAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
@@ -20,28 +20,25 @@ public class CoreNLPExample {
         BasicConfigurator.configure(new NullAppender());
 
         // Initialize and annotate document pipeline
-        String text = "Obama was born in Hawaii. He is our president. " +
-                "President Xi Jinping of China, on his first visit to the United States, " +
-                "showed off his familiarity with American history and pop culture on Tuesday night.";
+        Annotation document = new Annotation("Barack Obama was born in Hawaii. He is the president. Obama was elected in 2008.");
         String annotators = "tokenize, ssplit, pos, lemma, ner, parse, natlog, openie, mention, dcoref";
-        Annotation document = new Annotation(text);
         StanfordCoreNLP pipeline = new StanfordCoreNLP(PropertiesUtils.asProperties("annotators", annotators));
         pipeline.annotate(document);
 
         // Explore annotations
         System.out.println("Coreferences: " + document.get(CorefCoreAnnotations.CorefChainAnnotation.class));
 
-        for (CoreMap sentence: document.get(CoreAnnotations.SentencesAnnotation.class)) {
-            System.out.println("\nSentence: " + sentence.get(CoreAnnotations.TextAnnotation.class));
+        for (CoreMap sentence: document.get(SentencesAnnotation.class)) {
+            System.out.println("\nSentence: " + sentence.get(TextAnnotation.class));
 
-            for (CoreLabel word: sentence.get(CoreAnnotations.TokensAnnotation.class)) {
-                System.out.println("Word: " + word.get(CoreAnnotations.TextAnnotation.class) +
-                        ", Lemma: " + word.get(CoreAnnotations.LemmaAnnotation.class) +
-                        ", POS tag: " + word.get(CoreAnnotations.PartOfSpeechAnnotation.class) +
-                        ", NER tag: " + word.get(CoreAnnotations.NamedEntityTagAnnotation.class));
+            for (CoreLabel word: sentence.get(TokensAnnotation.class)) {
+                System.out.println("Word: " + word.get(TextAnnotation.class) +
+                        ", Lemma: " + word.get(LemmaAnnotation.class) +
+                        ", POS tag: " + word.get(PartOfSpeechAnnotation.class) +
+                        ", NER tag: " + word.get(NamedEntityTagAnnotation.class));
             }
 
-            System.out.println("Mentions: " + sentence.get(CoreAnnotations.MentionsAnnotation.class));
+            System.out.println("Mentions: " + sentence.get(MentionsAnnotation.class));
             System.out.println("OpenIE: " + sentence.get(NaturalLogicAnnotations.RelationTriplesAnnotation.class));
             System.out.println("Dependency parse: " + sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class));
             System.out.println("Constituency parse: ");
--- src/main/java/SimpleCoreNLPExample.java
+++ src/main/java/SimpleCoreNLPExample.java
@@ -1,9 +1,7 @@
 // Exploration of the Stanford Simple CoreNLP API: stanfordnlp.github.io/CoreNLP/simple
-
-import edu.stanford.nlp.simple.Document;
-import edu.stanford.nlp.simple.Sentence;
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.varia.NullAppender;
+import edu.stanford.nlp.simple.*;
 
 public class SimpleCoreNLPExample {
     public static void main(String[] args) {
@@ -11,10 +9,7 @@ public class SimpleCoreNLPExample {
         BasicConfigurator.configure(new NullAppender());
 
         // Initialize document
-        String text = "Obama was born in Hawaii. He is our president. " +
-                "President Xi Jinping of China, on his first visit to the United States, " +
-                "showed off his familiarity with American history and pop culture on Tuesday night.";
-        Document doc = new Document(text);
+        Document doc = new Document("Barack Obama was born in Hawaii. He is the president. Obama was elected in 2008.");
 
         // Explore annotations (lazy loaded)
         for (Sentence sentence : doc.sentences()) {
@@ -23,8 +18,9 @@ public class SimpleCoreNLPExample {
             System.out.println("Lemmas: " + sentence.lemmas());
             System.out.println("POS tags: " + sentence.posTags());
             System.out.println("NER tags: " + sentence.nerTags());
-            System.out.println("Parse: " + sentence.parse());
-            System.out.println("Dependencies: " + sentence.incomingDependencyLabels());
+            System.out.println("Dependency parse: " + sentence.incomingDependencyLabels());
+            System.out.println("Constituency parse: " + sentence.parse());
+            System.out.println("Natural logic polarity: " + sentence.natlogPolarities());
             System.out.println("Open Information Extraction: " + sentence.openie());
         }
     }
