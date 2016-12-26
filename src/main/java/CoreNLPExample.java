b1de592c06804d51015bd4c1bcf8097f090e8105
Charles <charlescc9@yahoo.com>
2016-12-26 14:37:08 -0700

    Added sentiment and mention annotations

--- src/main/java/CoreNLPExample.java
+++ src/main/java/CoreNLPExample.java
@@ -1,12 +1,13 @@
 // Exploration of the Stanford CoreNLP API: stanfordnlp.github.io/CoreNLP/api
 
-import edu.stanford.nlp.dcoref.CorefCoreAnnotations;
+import edu.stanford.nlp.dcoref.CorefCoreAnnotations.*;
 import edu.stanford.nlp.ling.CoreAnnotations.*;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.naturalli.NaturalLogicAnnotations;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
 import edu.stanford.nlp.semgraph.SemanticGraphCoreAnnotations;
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations;
 import edu.stanford.nlp.trees.TreeCoreAnnotations;
 import edu.stanford.nlp.trees.TreePrint;
 import edu.stanford.nlp.util.CoreMap;
@@ -21,16 +22,15 @@ public class CoreNLPExample {
 
         // Initialize and annotate document pipeline
         Annotation document = new Annotation("Barack Obama was born in Hawaii. He is the president. Obama was elected in 2008.");
-        String annotators = "tokenize, ssplit, pos, lemma, ner, parse, natlog, openie, mention, dcoref";
+        String annotators = "tokenize, ssplit, pos, lemma, ner, parse, entitymentions, natlog, openie, sentiment";
         StanfordCoreNLP pipeline = new StanfordCoreNLP(PropertiesUtils.asProperties("annotators", annotators));
         pipeline.annotate(document);
 
         // Explore annotations
-        System.out.println("Coreferences: " + document.get(CorefCoreAnnotations.CorefChainAnnotation.class));
+        System.out.println("Coreferences: " + document.get(CorefChainAnnotation.class));
 
         for (CoreMap sentence: document.get(SentencesAnnotation.class)) {
             System.out.println("\nSentence: " + sentence.get(TextAnnotation.class));
-
             for (CoreLabel word: sentence.get(TokensAnnotation.class)) {
                 System.out.println("Word: " + word.get(TextAnnotation.class) +
                         ", Lemma: " + word.get(LemmaAnnotation.class) +
@@ -40,6 +40,7 @@ public class CoreNLPExample {
 
             System.out.println("Mentions: " + sentence.get(MentionsAnnotation.class));
             System.out.println("OpenIE: " + sentence.get(NaturalLogicAnnotations.RelationTriplesAnnotation.class));
+            System.out.println("Sentiment: " + sentence.get(SentimentCoreAnnotations.SentimentClass.class));
             System.out.println("Dependency parse: " + sentence.get(SemanticGraphCoreAnnotations.BasicDependenciesAnnotation.class));
             System.out.println("Constituency parse: ");
             TreePrint treePrint = new TreePrint("penn");
